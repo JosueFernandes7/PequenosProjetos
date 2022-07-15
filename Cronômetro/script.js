7a58bc54347faada196c161bd53c2c9be9256c7c
Josue Fernandes <josue7mf@hotmail.com>
2022-07-15 01:26:09 -0300

    ajustes de tempo

--- Cronômetro/script.js
+++ Cronômetro/script.js
@@ -1,30 +1,40 @@
-const start = document.querySelector('.start');
-const stop = document.querySelector('.stop');
-const restart = document.querySelector('.restart');
-const timerText = document.querySelector('.timer-text');
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const restart = document.querySelector(".restart");
+const timerText = document.querySelector(".timer-text");
 
 let i = 0;
+let seg, min, h;
 let timer;
 
-start.addEventListener('click', startTimer);
-stop.addEventListener('click', stopTimer);
-restart.addEventListener('click', restartTimer);
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+restart.addEventListener("click", restartTimer);
 
 function startTimer() {
-  console.log(!timerText.hasAttribute('on'))
-  if(!timerText.hasAttribute('on')){
+  console.log(!timerText.hasAttribute("on"));
+  if (!timerText.hasAttribute("on")) {
     timer = setInterval(() => {
-      timerText.innerText = i++ + "s";
-    },1000)
+      h = Math.floor(i / 3600);
+      let gasto = i - h * 3600; 
+      min = Math.floor((gasto) / 60);
+      seg = Math.floor((gasto) % 60);
+
+      if (i <= 9) seg = "0" + i;
+      if (min <= 9) min = "0" + min;
+      if (h <= 9) h = "0" + h;
+
+      timerText.innerText = `${h}:${min}:${seg}`;
+      i++;
+    }, 1000);
   }
-  timerText.setAttribute('on', '');
+  timerText.setAttribute("on", "");
 }
 function stopTimer() {
-  timerText.removeAttribute('on');
+  timerText.removeAttribute("on");
   clearInterval(timer);
 }
 function restartTimer() {
-
-  i=0;
-  timerText.innerText = 0;
+  i = 0;
+  timerText.innerText = "00:00:00";
 }
